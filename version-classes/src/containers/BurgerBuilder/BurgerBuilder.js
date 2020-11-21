f7316c6a0170ee9967f98885f7c16ec0775fe22c
awpala <adam.w.pala@gmail.com>
2020-11-21 10:57:11 -0600

    Add basic BurgerBuilder logic

--- version-classes/src/containers/BurgerBuilder/BurgerBuilder.js
+++ version-classes/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 
 import Aux from '../../hoc/Auxiliary';
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
     render() {
         return (
             <Aux>
-                <div></div>
+                <Burger />
                 <div></div>
             </Aux>
         );
