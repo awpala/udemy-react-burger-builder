69a2799f3c9df589dd51bee6527d3a6943ef2b77
awpala <adam.w.pala@gmail.com>
2020-11-21 10:26:06 -0600

    Initialize BurgerBuilder component

--- version-classes/src/components/Layout/Layout.js
+++ version-classes/src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
+
 import Aux from '../../hoc/Auxiliary';
+import classes from './Layout.css';
 
 const layout = ({ children }) => (
     <Aux>
         <div></div>
-        <main>
+        <main className={classes.Content}>
             {children}
         </main>
     </Aux>
---
+++ version-classes/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Aux from '../../hoc/Auxiliary';
+
+class BurgerBuilder extends Component {
+    render() {
+        return (
+            <Aux>
+                <div></div>
+                <div></div>
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
