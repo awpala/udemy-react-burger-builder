08dc6dabd40673d6b649246b93f291f4914007df
awpala <adam.w.pala@gmail.com>
2020-12-05 10:15:41 -0600

    Add zero ingredients use case

--- version-classes/src/components/Burger/Burger.js
+++ version-classes/src/components/Burger/Burger.js
@@ -4,11 +4,18 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = ({ ingredients }) => {
-    const transformedIngredients = Object.keys(ingredients).map(igKey => {
+    let transformedIngredients = Object.keys(ingredients).map(igKey => {
         return [...Array(ingredients[igKey])].map((_, i) => {
             return <BurgerIngredient key={igKey + i} type={igKey} />
         });
-    });
+    })
+    .reduce((arr, el) => {
+        return arr.concat(el);
+    }, []);
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>;
+    }
 
     return (
         <div className={classes.Burger}>
