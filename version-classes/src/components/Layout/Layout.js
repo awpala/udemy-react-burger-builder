8d57b7cecd770b7e96197c6028937f1ef2c83e8c
awpala <adam.w.pala@gmail.com>
2020-12-06 17:44:49 -0600

    Add backdrop to side drawer component

--- version-classes/src/components/Layout/Layout.js
+++ version-classes/src/components/Layout/Layout.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../hoc/Auxiliary';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = ({ children }) => (
-    <Aux>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.Content}>
-            {children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
+    
+    sideDrawerClosedHandler = () => {
+        this.setState({ showSideDrawer: false });
+    }
 
-export default layout;
+    render() {
+        return (
+            <Aux>
+                <Toolbar />
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler}
+                />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        );
+    }
+}; 
+
+export default Layout;
--- version-classes/src/components/Navigation/SideDrawer/SideDrawer.js
+++ version-classes/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,18 +3,28 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Auxiliary';
 
-const sideDrawer = (props) => {
+const sideDrawer = ({ open, closed }) => {
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+
+    if (open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
 
     return (
-        <div className={classes.SideDrawer} >
-            <div className={classes.Logo} >
-                <Logo />
+        <Aux>
+            <Backdrop show={open} clicked={closed} />
+            <div className={attachedClasses.join(' ')} >
+                <div className={classes.Logo} >
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </Aux>
     );
 };
 
