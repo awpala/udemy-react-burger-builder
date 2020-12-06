9227d8a6da6f0b682b153152599b3d091fa20bda
awpala <adam.w.pala@gmail.com>
2020-12-06 15:53:39 -0600

    Add toolbar component

--- version-classes/src/components/Layout/Layout.js
+++ version-classes/src/components/Layout/Layout.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 import Aux from '../../hoc/Auxiliary';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = ({ children }) => (
     <Aux>
-        <div></div>
+        <Toolbar />
         <main className={classes.Content}>
             {children}
         </main>
