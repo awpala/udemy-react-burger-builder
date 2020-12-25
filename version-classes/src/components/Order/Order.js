7d091332bb0f36196706196181ac78a1a3a86b80
awpala <adam.w.pala@gmail.com>
2020-12-25 11:18:53 -0600

    Add fetching orders via backend and output

--- version-classes/src/components/Order/Order.js
+++ version-classes/src/components/Order/Order.js
@@ -2,11 +2,39 @@ import React from 'react';
 
 import classes from './Order.css';
 
-const order = (props) => (
-    <div className={classes.Order}>
-        <p></p>
-        <p>Price: <strong></strong></p>
-    </div>
-);
+const order = ({ ingredients, price }) => {
+    const mappedIngredients = [];
+
+    for (let ingredientName in ingredients) {
+        mappedIngredients.push({
+            name: ingredientName,
+            amount: ingredients[ingredientName],
+        });
+    }
+
+    const ingredientsOutput = mappedIngredients.map(ig => (
+        <span
+            key={ig.name}
+            style={{ 
+                textTransform: 'capitalize',
+                display: 'inline-block',
+                margin: '0 8px',
+                border: '1px solid #ccc',
+                padding: '5px'
+            }}
+        >
+            {ig.name} ({ig.amount})
+        </span>
+        
+        )
+    );
+
+    return (
+        <div className={classes.Order}>
+            <p>Ingredients: {ingredientsOutput}</p>
+            <p>Price: <strong>USD {Number.parseFloat(price).toFixed(2)}</strong></p>
+        </div>
+    );
+}
 
 export default order;
