c9e392b02338f321621f4c47093dcbda427f4811
awpala <adam.w.pala@gmail.com>
2020-12-27 14:38:26 -0600

    Use env variable to only expose Redux dev tools in development

--- version-classes/src/index.js
+++ version-classes/src/index.js
@@ -12,12 +12,9 @@ import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import orderReducer from './store/reducers/order';
 import authReducer from './store/reducers/auth';
 
-const composeEnhancers = (
-    process.env.NODE_ENV === 'development'
-        ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ // for Redux Dev Tools browser extension (cf. https://github.com/reduxjs/redux-thunk)
-        : null
-    )
-    || compose;
+const composeEnhancers = process.env.NODE_ENV === 'development'
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ // for Redux Dev Tools browser extension (cf. https://github.com/reduxjs/redux-thunk)
+    : (null || compose);
 
 const rootReducer = combineReducers({
     burgerBuilder: burgerBuilderReducer,
